ef70241f46caed11d9054729a3959055c6f51623
chengjiang <15712859926@163.com>
2019-02-15 14:16:18 +0800

    修改

--- lib/bottom_appBar_demo.dart
+++ lib/bottom_appBar_demo.dart
@@ -1,15 +1,31 @@
 import 'package:flutter/material.dart';
+import 'each_view.dart';
 
 class BottomAppBarDemo extends StatefulWidget {
   _BottomAppBarDemoState createState() => _BottomAppBarDemoState();
 }
 
 class _BottomAppBarDemoState extends State<BottomAppBarDemo> {
+  List<Widget> _eachView;
+  int _index = 0;
+  @override
+  void initState() {
+    super.initState();
+    _eachView = List();
+    _eachView..add(EachView('Home'))..add(EachView('JSPang'));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: _eachView[_index],
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          Navigator.of(context)
+              .push(MaterialPageRoute(builder: (BuildContext context) {
+            return EachView('New Page');
+          }));
+        },
         tooltip: 'JSPang',
         child: Icon(
           Icons.add,
@@ -27,12 +43,20 @@ class _BottomAppBarDemoState extends State<BottomAppBarDemo> {
             IconButton(
               icon: Icon(Icons.home),
               color: Colors.white,
-              onPressed: () {},
+              onPressed: () {
+                setState(() {
+                  _index = 0;
+                });
+              },
             ),
             IconButton(
               icon: Icon(Icons.airport_shuttle),
               color: Colors.white,
-              onPressed: () {},
+              onPressed: () {
+                setState(() {
+                  _index = 1;
+                });
+              },
             ),
           ],
         ),
